2adb8a5d4d14633312a0b63da21d6905884bd626
Yan Soares <yts.1995@gmail.com>
2021-05-18 20:05:03 -0300

    chore: add tables migrations

--- database/migrations/2021_04_14_105302_create_transactions_table.php
+++ database/migrations/2021_04_14_105302_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('cpf_cnpj')->unique();
-            $table->integer('user_type');
-            $table->string('password');
+            $table->decimal('value');
+            $table->integer('transaction_type');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 }
