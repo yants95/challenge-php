230a42355b0fc6015371f3031f5b6ef9d9ffee77
Yan Soares <yts.1995@gmail.com>
2021-05-18 20:05:43 -0300

    chore: add models factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,26 +22,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $userType = random_int(0,1);
+        $documentId = ($userType) ? $this->faker->cnpj(false) : $this->faker->cpf(false);
+
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'user_type' => $userType,
+            'document_id' => $documentId,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
