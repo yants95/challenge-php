7e7f3f5b9d8928ec53b7a50cfc9449b092908b40
Yan Soares <yts.1995@gmail.com>
2021-05-18 20:41:40 -0300

    chore: add user repository

---
+++ app/Repositories/Implementations/UserRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Enums\UserType;
+use App\Models\User;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+    private $user;
+
+    public function __construct()
+    {
+        $this->user = new User();
+    }
+
+    public function checkPayerType($userId)
+    {
+        $person = $this->user->find($userId);
+
+        return $person->user_type === UserType::COMMON_PERSON;
+    }
+}
