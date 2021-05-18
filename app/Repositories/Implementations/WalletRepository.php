0be53a56ef2e08048aa88e4bad71f0f7daff9694
Yan Soares <yts.1995@gmail.com>
2021-05-18 20:42:59 -0300

    feat: implement more methods on wallet repository

--- app/Repositories/Implementations/WalletRepository.php
+++ app/Repositories/Implementations/WalletRepository.php
@@ -4,18 +4,32 @@ namespace App\Repositories;
 
 use App\Models\Wallet;
 use App\Repositories\Interfaces\WalletRepositoryInterface;
-
 class WalletRepository implements WalletRepositoryInterface
 {
     private $wallet;
 
-    public function __construct()
+    public function __construct(Wallet $wallet)
     {
-        $this->wallet = new Wallet();
+        $this->wallet = $wallet;
     }
 
     public function getWalletByUser(int $userId)
     {
         return $this->wallet->find($userId);
+    }
+
+    public function getSufficientWalletAmount($amountTransfer)
+    {
+        return $this->wallet->amount >= $amountTransfer;
+    }
+
+    public function increaseAmount(Wallet $wallet, $amount)
+    {
+        return $wallet->amount += $amount;
+    }
+
+    public function decreaseAmount(Wallet $wallet, $amount)
+    {
+        return $wallet->amount -= $amount;
     }
 }
--- app/Repositories/Interfaces/WalletRepositoryInterface.php
+++ app/Repositories/Interfaces/WalletRepositoryInterface.php
@@ -2,7 +2,12 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Models\Wallet;
+
 interface WalletRepositoryInterface
 {
     public function getWalletByUser(int $userId);
+    public function getSufficientWalletAmount($amountTransfer);
+    public function increaseAmount(Wallet $payeeWallet, $amount);
+    public function decreaseAmount(Wallet $payerWallet, $amount);
 }
