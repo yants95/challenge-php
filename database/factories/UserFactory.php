631799c8bb5d68c8de48a5045af1d5ed969e736a
Yan Soares <yts.1995@gmail.com>
2021-05-20 13:38:25 -0300

    chore: remove some fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,8 +30,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'user_type' => $userType,
-            'cpf_cnpj' => $cpfCnpj,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+            'cpf_cnpj' => $cpfCnpj
         ];
     }
 }
