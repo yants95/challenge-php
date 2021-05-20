2e1d65755211c4fab989dbb9539530eb68fddfa5
Yan Soares <yts.1995@gmail.com>
2021-05-20 13:40:43 -0300

    improve: improve mocky authorization implementation

--- src/App/Core/External/Implementations/MockyAuthorizationService.php
+++ src/App/Core/External/Implementations/MockyAuthorizationService.php
@@ -3,15 +3,15 @@
 namespace App\Core\External\Implementations;
 
 use App\Core\External\Interfaces\AuthorizationServiceInterface;
+use Illuminate\Support\Facades\Http;
 
 class MockyAuthorizationService implements AuthorizationServiceInterface
 {
     public function authorize()
     {
-        $client = new \GuzzleHttp\Client(['base_uri' => 'https://run.mocky.io']);
+        $request = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
+        $response = json_decode($request, true);
 
-        $response = $client->request('GET', '/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6', [ 'allow_redirects' => false ]);
-
-        return $response->getStatusCode() === 200;
+        return $response["message"] === 'Autorizado';
     }
 }
